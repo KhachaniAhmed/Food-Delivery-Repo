706e0e0be0d25ad83918ea4d0c662417a0b0e259
AitBassouAli <aitbassouali@gmail.com>
2018-11-04 22:12:33 +0000

    Ading Restaurant CRUD Operations

--- food-Delivery-App/src/main/java/org/mql/metier/IRestaurantMetier.java
+++ food-Delivery-App/src/main/java/org/mql/metier/IRestaurantMetier.java
@@ -1,5 +1,14 @@
 package org.mql.metier;
 
+import java.util.List;
+
+import org.mql.entities.Restaurant;
+
 public interface IRestaurantMetier {
 
+	public List<Restaurant> getAll();
+	
+	public void add(Restaurant restaurant);
+	
+	public void delete(Restaurant restaurant);
 }
--- food-Delivery-App/src/main/java/org/mql/metier/RestaurantImpl.java
+++ food-Delivery-App/src/main/java/org/mql/metier/RestaurantImpl.java
@@ -1,5 +1,30 @@
 package org.mql.metier;
 
+import java.util.List;
+
+import org.mql.dao.RestaurantRepository;
+import org.mql.entities.Restaurant;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class RestaurantImpl implements IRestaurantMetier {
+
+	@Autowired
+	private RestaurantRepository restaurantRepository;
+	
+	@Override
+	public List<Restaurant> getAll() {
+		return restaurantRepository.findAll();
+	}
+	
+	@Override
+	public void add(Restaurant restaurant) {
+		restaurantRepository.save(restaurant);
+	}
+
+	@Override
+	public void delete(Restaurant restaurant) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
